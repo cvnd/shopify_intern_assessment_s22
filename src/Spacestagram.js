6562dbfd98a81adb2b8f6d30294807996701c310
cvnd <claudiavdioneda@gmail.com>
2022-01-10 14:10:20 -0500

    change to apod and have responsive image tiling

--- src/Spacestagram.js
+++ src/Spacestagram.js
@@ -4,39 +4,41 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types'
 var settings = require('./config.json');
 
-const ROVERS = ['curiosity', 'spirit', 'opportunity'];
-
-const fetchAPI = (user, url) => {
+const ROVERS = ['curiosity', 'opportunity', 'spirit'];
+const PAGE_LIMIT = 25;
+const fetchIMGS = (params) => {
   return (
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${user}/photos?${url}&api_key=${settings.api_key}`, {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${settings.api_key}&${params}`, {
       method: 'GET',
     })
     .then(response => response.json())
-    .then(json => {
-      return json;
+    .then(data => {
+      const arr = [];
+      data.forEach(d => {
+        if(d.media_type !== "video") {
+          arr.push(d);
+        }
+      })
+      return arr;
     })
   )
 };
 
-const fetchManifest = (rover) => {
-  return (
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}?api_key=${settings.api_key}`, {
-      method: 'GET',
-    })
-    .then(response => response.json())
-    .then(json => {
-      return json;
-    })
-  )
-
+const formatDate = (date) => {
+  return date.toISOString().split('T')[0];
 }
 
 const Card = ({data, liked}) => {
   
   return (
-    <div class="card">
-      <div>HELLO</div>
-      <div></div>
+    <div className="card">
+      {/* <div className="card-title">{data.copyright || "NASA"}</div> */}
+      {/* <div className="card-photo"> */}
+      <img className="card-photo" src={data.hdrul || data.url}/>
+      {/* </div> */}
+      {/* <h5>
+        Taken on {data.date}
+      </h5> */}
     </div>
   );
 }
@@ -50,50 +52,58 @@ Card.defaultProps = {
   liked: false
 }
 
-const Viewer = ({rover}) => {
-  const [photos, getPhotos] = useState();
-};
-
-Viewer.propTypes = {
-  rover: PropTypes.string.isRequired
-}
-
-
 const Spacestagram = () => {
-  const [images, setImages] = useState();
-  const [manifests, setManifests] = useState([]);
-  const [fetchingManifests, setFetchingManifests] = useState(false);
-  const [roverFocus, setRoverFocus] = useState(null);
-  useEffect(() => {
-    console.log(manifests);
-    if(!fetchingManifests && manifests.length === 0) {
-      setFetchingManifests(true);
-      console.log("Fetching rovers");
-      const newManifests = [];
-      ROVERS.forEach(async (rover) => {
-        console.log(`Rover name: ${rover}`);
-        newManifests.push(await fetchManifest(rover));
-      });
+  const [photos, setPhotos] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [oldest, setOldest] = useState(new Date());
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-      setManifests(newManifests);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    if(isFetching) {
+      const end_date = oldest;
+      const start_date = new Date(end_date);
+      start_date.setDate(oldest.getDate() - PAGE_LIMIT + 1);
+      fetchIMGS(`end_date=${formatDate(end_date)}&start_date=${formatDate(start_date)}`).then(data => {
+        setIsFetching(false);
+        if(photos) {
+          setPhotos([...photos, ...data.reverse()]);
+        } else {
+          setPhotos(data.reverse());
+        }
+
+        setOldest(start_date);
+      });
     }
-    console.log(manifests);
-  }, [fetchingManifests, manifests]);
+  }, [oldest, isFetching, photos]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   
+  useEffect(() => {
+    if(scrollPosition / window.innerHeight > 0.60 && !isFetching) {
+      setIsFetching(true);
+    }
+  }, [scrollPosition]);
+  
+
   return (
     <div className="Spacestagram">
-      <div id="nav">
-        <h4>Select a rover.</h4>
-        <div>
-          {Object.values(ROVERS).map(val => {
-            return( <div onClick={() => setRoverFocus(val)}className="rover-selection button">{val}</div>);
-          })}
-        </div>
-      </div>
-      <div id="content">
-          <Viewer rover={roverFocus}/>
-      </div>
+      <main>
+        {photos && Object.values(photos).map((photo) => {
+          return(<Card data={photo}/>)
+        })}
+      </main>
     </div>
   );
 }
