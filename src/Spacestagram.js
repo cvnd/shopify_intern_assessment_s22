c499b3c30e42e4c9b1e8b7af19f3a35be7bd90d0
cvnd <claudiavdioneda@gmail.com>
2022-01-11 20:06:23 -0500

    date calendar? and gh-pages

--- src/Spacestagram.js
+++ src/Spacestagram.js
@@ -2,19 +2,27 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faHeart as fHeart, faRocket, faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faCog, faHeart as fHeart, faRocket, faSearch, faTimes, faArrowLeft, faArrowRight, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as oHeart } from '@fortawesome/free-regular-svg-icons'
-import { tab } from '@testing-library/user-event/dist/tab';
+import DatePicker from 'react-datepicker';
 
 var settings = require('./config.json');
 
 const PAGE_LIMIT = 25;
-
+const DATE_ASCENDING = 0;
+const DATE_DESCENDING = 1;
+const CURRENT_DATE = new Date();
 
 const formatDate = (date) => {
   return date.toISOString().split('T')[0];
 }
 
+function range(a, b) {
+  if(b < a) {
+    return null;
+  }
+  return [...Array(b - a).keys()].map(i => i + a);
+}
 const Card = ({data, toggleModal, setModalData, tabIndex}) => {
   const [isLiked, setIsLiked] = useState(false);
 
@@ -103,19 +111,99 @@ PhotoModal.defaultProps = {
   tabIndex: 0
 }
 
+const Calendar = ({currentDate, setDate}) =>{
+  const years = range(1995, CURRENT_DATE.getYear() + 1901);
+  console.log(years);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return (
+    <DatePicker
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className="calendar-header-inner">
+          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <div className='calendar-header-selection'>
+            <select
+              style={{fontSize: "0.9em", backgroundColor: "transparent", border: "none"}}
+              value={months[date.getMonth()]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => {
+                console.log(date.getYear());
+                if(date.getYear() === 95 && !months.includes(option, 5)) {
+                  return null;
+                }
+                return (<option key={option} value={option}>
+                  {option}
+                </option>)
+              })}
+            </select>
+            <select
+              style={{fontSize: "0.9em", backgroundColor: "transparent", border: "none"}}
+              value={date.getYear() + 1900}
+              onChange={({ target: { value } }) => changeYear(value)}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+      )}
+      selected={currentDate}
+      onChange={(date) => setDate(date)}
+      showPreviousMonths={false}
+      minDate={new Date("1995-06-16")}
+      maxDate={CURRENT_DATE}
+    />
+  );
+}
+
+Calendar.propTypes = {
+  currentDate: PropTypes.object,
+  setDate: PropTypes.func.isRequired,
+}
 
 const Spacestagram = () => {
   const [photos, setPhotos] = useState(null);
   const [isFetching, setIsFetching] = useState(true);
-  const [oldest, setOldest] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
   const [scrollPosition, setScrollPosition] = useState(0);
   const [modalData, setModalData] = useState();
   const [modalVisibility, setModalVisibility] = useState(false);
   const [error, setError] = useState(null);
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    setScrollPosition(position);
-  };
+  const [sort, setSort] = useState(DATE_DESCENDING);
+  const [filterError, setFilterError] = useState(null);
+  
   const fetchIMGS = (params) => {
     return (
       fetch(`https://api.nasa.gov/planetary/apod?api_key=${settings.api_key}&${params}`, {
@@ -143,28 +231,34 @@ const Spacestagram = () => {
       })
     )
   };
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
   useEffect(() => {
     console.log(photos);
     if(isFetching) {
-      const end_date = oldest;
+      const end_date = endDate;
       if(photos) {
         end_date.setDate(end_date.getDate() - 1);
       }
       const start_date = new Date(end_date);
 
-      start_date.setDate(oldest.getDate() - PAGE_LIMIT);
-      fetchIMGS(`end_date=${formatDate(end_date)}&start_date=${formatDate(start_date)}`).then(data => {
-        setIsFetching(false);
-        setOldest(start_date);
-        if(photos) {
-          setPhotos([...photos, ...data.reverse()]);
-        } else {
-          setPhotos(data.reverse());
-        }
+      start_date.setDate(endDate.getDate() - PAGE_LIMIT);
+      // fetchIMGS(`end_date=${formatDate(end_date)}&start_date=${formatDate(start_date)}`).then(data => {
+      //   setIsFetching(false);
+      //   setEndDate(start_date);
+      //   if(photos) {
+      //     setPhotos([...photos, ...data.reverse()]);
+      //   } else {
+      //     setPhotos(data.reverse());
+      //   }
 
-      });
+      // });
     }
-  }, [oldest, isFetching, photos]);
+  }, [sort, startDate, endDate, isFetching, photos]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
@@ -196,15 +290,44 @@ const Spacestagram = () => {
 
   }
 
+  useEffect(() => {
+    if(endDate < startDate) {
+      setFilterError("End date can't be earlier than start date");
+    }
+  }, [startDate, endDate]);
+
   return (
     <div className="Spacestagram">
       <PhotoModal data={modalData} visible={modalVisibility} setVisible={setModalVisibility}/>
       <header>
-        <div>
+        <div id='header-siteinfo'>
           <FontAwesomeIcon icon={faRocket}/>
           <h1>spacestagram</h1>
         </div>
         <p><i>brought to you by NASA</i></p>
+        <div id='controls'>
+          <ul className='controls-options'>
+            <li className="sort-selection">
+              <label>Sort by</label>
+              <select 
+                style={{fontSize: "0.8em", padding: "0.5em", backgroundColor: "#e1e1e1", border: "none"}}
+                value={sort}
+                onChange={(ev) => setSort(ev.target.value)}
+              >
+                <option value={DATE_DESCENDING}>Newest</option>
+                <option value={DATE_ASCENDING}>Oldest</option>
+              </select>
+            </li>
+            <li className="date-selection">
+              <label>To</label>
+              <Calendar currentDate={startDate} setDate={(date) => setStartDate(date)}/>
+              {/* <DatePicker dateFormat="yyyy/MM/dd" selected={startDate} onChange={(date) => console.log(date)} /> */}
+              <label>From</label>
+              {/* <DatePicker dateFormat="yyyy/MM/dd" selected={endDate} onChange={(date) => console.log(date)} /> */}
+              <Calendar currentDate={endDate} setDate={(date) => setEndDate(date)}/>
+            </li>
+          </ul>
+        </div>
       </header>
       <main >
         {photos && Object.values(photos).map((photo) => {
